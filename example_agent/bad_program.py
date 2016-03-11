3f7040e4da1376900673d28360066a240c7f11df
Mike <kjyoung@ualberta.ca>
2016-03-11 10:49:11 -0700

    added bad_program.py which frequently timesout for testing purposes

---
+++ example_agent/bad_program.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from gtpinterface import gtpinterface
+from bad_agent import mctsagent
+def main():
+	"""
+	Main function, simply sends user input on to the gtp interface and prints
+	responses.
+	"""
+	agent = mctsagent()
+	interface = gtpinterface(agent)
+	while True:
+		command = input()
+		success, response = interface.send_command(command)
+		print(("= " if success else "? ")+response+'\n')
+
+if __name__ == "__main__":
+	main()
